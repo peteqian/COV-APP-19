0e86fd25a3a005d696681a2ead6297a249a07ed2
thepancakeman <lhq036@uowmail.edu.au>
2021-04-21 17:50:58 +1000

    Version 1.0.0
    Basic Implementation so far:
    1. Sign in
    2. Create Account
    3. Create a user profile (unsure)

--- dataInput.py
+++ dataInput.py
@@ -1,4 +1,43 @@
+# History: 21-04-2021
+# Version 1.0.0
+# dataInput.py file is main to act as the layer for users to provide their
+# data. This layer should only be concerned in obtaining and storing data.
+
+
 import csv
+from passlib.hash import pbkdf2_sha256
+from datetime import date
+user_counter = 0
+
+
+def create_account(_firstname, _lastname, _email, password):
+    # print(_firstname, _lastname, _email, password)
+    # print(hash)
+    # print(pbkdf2_sha256.verify(password, hash))
+
+    hash = pbkdf2_sha256.hash(password)
+
+    global user_counter
+    user_counter += 1
+
+    my_list = [str(user_counter), _firstname, _lastname, _email, hash, "\n"]
+    my_string = ':'.join(my_list)
+
+    file = open("stored-accounts.txt", "a")
+    file.write(my_string)
+    file.close()
+
+
+def manual_checking_in(_firstname, _lastname, _email, _mobile, _location):
+
+    today = date.today()
+
+    my_list = [_firstname, _lastname, _email, str(_mobile), _location, str(today)]
+    my_string = ':'.join(my_list)
+
+    file = open("checking_in_records.txt", "a")
+    file.write(my_string)
+    file.close()
 
 
 def filepathexists(fileName):
@@ -21,8 +60,8 @@ def readfile(fileName):
             dob = row[2]
             email = row[3]
             mobile = row[4]
-            address = row [5]
-            occupation = row [6]
+            address = row[5]
+            occupation = row[6]
             print(row)
 
 
@@ -34,6 +73,3 @@ def printData():
     print(f'mobile is:  {mobile}')
     print(f'address is:  {address}')
     print(f'occupation is:  {occupation}')
-
-
-
--- main.py
+++ main.py
@@ -4,8 +4,10 @@
 import dataInput as dataInput
 
 if __name__ == '__main__':
-    filename = "sample-data.txt"
-    dataInput.filepathexists(filename)
-    dataInput.readfile(filename)
-    dataInput.printData()
-
+    # filename = "sample-data.txt"
+    # dataInput.filepathexists(filename)
+    # dataInput.readfile(filename)
+    # dataInput.printData()
+    dataInput.create_account("Peter", "Qian", "lhq036@uowmail.edu.au", "pancakes")
+    dataInput.create_account("Denzel", "Whitfield", "lhq036@uowmail.edu.au", "waffles")
+    dataInput.manual_checking_in("Peter", "Qian","lhq036@uowmail.edu.au", 0411111111, "Anne's House")
