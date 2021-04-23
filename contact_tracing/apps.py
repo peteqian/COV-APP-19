fbdcb8a6a5363ce5284008a4be7c1b8a589cf9f7
Ryan Wickham <48148988+RyanWickham@users.noreply.github.com>
2021-04-23 20:51:18 +1000

    Add files via upload

---
+++ contact_tracing/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ContactTracingConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'contact_tracing'
---
+++ web_project/urls.py
@@ -0,0 +1,25 @@
+"""web_project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+from contact_tracing.views import api_request_handler
+
+urlpatterns = [
+    path('', api_request_handler.index),
+    path('generalPublic', api_request_handler.add_general_user),
+    path('<str:general_user_id>', api_request_handler.get_general_user),
+]
