b739dc997def8e618afb8bda90dad0694c8e6439
thepancakeman <lhq036@uowmail.edu.au>
2021-04-26 11:48:35 +1000

    Peter-Test

---
+++ bootcamp/urls.py
@@ -0,0 +1,39 @@
+"""bootcamp URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path, re_path # url
+from django.conf.urls.static import static
+
+from accounts.views import (
+    home_view,
+    login_view,
+    logout_view,
+    register_view,
+)
+
+
+urlpatterns = [
+    path(' ', home_view),
+    path('login/', login_view),
+    path('logout/', logout_view),
+    path('register/', register_view),
+    path('admin/', admin.site.urls),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
