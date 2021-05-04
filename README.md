941d488dc536cfe7e015599627823ce86cca3161
Peter Qian <60642461+peteqian@users.noreply.github.com>
2021-05-04 21:56:35 +1000

    clear

--- README.md
+++
@@ -1,6 +0,0 @@
-# COV-APP-19
-Just figuring out the core functionalities of the program
-
-1. Create Account
-2. Sign In
-3. Checking into a location
--- profiles/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Profile(models.Model):
-    content = models.TextField()
-    # image
-    # location
