038c87b2e4b4135c57f5e8e3f6882bd7fe6a5e48
AnthonySuker <30285783+AnthonySuker@users.noreply.github.com>
2021-05-19 15:40:31 +1000

    Implemented checkin api

--- covidTracing/visitinfo/api.py
+++ covidTracing/visitinfo/api.py
@@ -1,31 +1,44 @@
 
-from rest_framework import generics, permissions
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.response import Response
-from knox.models import AuthToken
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
 from .serializers import CheckInSerializer
+from knox.auth import TokenAuthentication
+from visitinfo.models import Visits, Locations
+from accounts.models import Accounts
 
 # Create your views here.
 
 
 
-class checkInAPI(generics.RetrieveAPIView):
+class checkInAPI(APIView):
     #verify token
-    permissions_classes = [permissions.IsAuthenticated, ] 
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
     serializer_class = CheckInSerializer
 
     def post(self, request, *args, **kwargs):
-
         # return Response({
         #     "message" : "help"
         # })
-        serializer = self.get_serializer(data=request.data)
+        serializer = CheckInSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
+        
+
+        uid = Accounts.objects.get(id=self.request.user.id) 
+        lid = Locations.objects.get(id=serializer.data["locationID"])
+
+        visit_instance = Visits.objects.create(user=uid, location=lid)
+        
         return Response({
             "data" : serializer.data
         })
-     
+    
+    def get(self, request, format=None):
+        content = {
+            'foo':'bar'
+        }
+        return Response(content)
 
 
     #associate token with user
--- covidTracing/visitinfo/serializers.py
+++ covidTracing/visitinfo/serializers.py
@@ -2,6 +2,8 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from django.contrib.auth import authenticate
+from knox.auth import TokenAuthentication as TA
+
 
 
 #nested serializers are read only
@@ -17,8 +19,13 @@ class CheckInSerializer(serializers.Serializer):
     locationID = serializers.CharField()
     dependents = dependentSerializer(many=True, required=False)
 
+
+#need to reimplement, ensure all data sent is valid
+#not used to validate user
+
     # def validate(self, data):
-    #     user = authenticate(**data)
+    #     user = TA.authenticate_credentials(key='token string').user
+    #     # user = authenticate(**data)
     #     if user and user.is_active:
     #         return user
     #     raise serializers.ValidationError("Incorrect Credentials")
