fc943f0a66aa8ec1ae07391f61fe3e54efd628a9
Scott Herford <scott.herford@live.com>
2021-05-21 12:54:23 +1000

    further edits to regBusiness. Also del bug?

--- covidTracing/accounts/api.py
+++ covidTracing/accounts/api.py
@@ -20,9 +20,11 @@ class RegisterBusinessAPI(generics.GenericAPIView):
     serializer_class = RegisterSerializer
     
     def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data, user_type='BUSINESS_USER')
+        serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
+        user.user_type = 'BUSINESS_USER'
+        user.save()
         return Response({
             "user": UserSerializer(user, context=self.get_serializer_context()).data, 
             "token": AuthToken.objects.create(user)[1]
